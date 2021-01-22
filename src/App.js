5945f817383929fa4086b174373a873e88a82134
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-01-22 14:35:46 +0330

    Final Session

--- src/App.js
+++ src/App.js
@@ -6,9 +6,48 @@ import MY_PROJECT_NAME, { MY_NAME } from './impoer-export-module/my-constant';
 import Student from './components/student/Student';
 
 class App extends Component {
-  
+  constructor() {
+    super();
+
+    this.state = {
+      studentList: [
+        {
+          firstName: 'Amir Hossein',
+          lastName: 'Salighe Dar',
+        },
+        {
+          firstName: 'Amir Mehdi',
+          lastName: 'Vaez',
+        },
+        {
+          firstName: 'Hassan',
+          lastName: 'Beiki',
+        },
+        {
+          firstName: 'Navid',
+          lastName: 'Farahmand',
+        },
+      ],
+      toggleShow: true,
+    };
+  }
   prompt = `Teacher is : ${MY_NAME}`;
 
+  clickHandler = () => {
+    console.log(this.state.studentList);
+    const newStudentList = [...this.state.studentList];
+    newStudentList[0].firstName = 'Mohammad';
+    newStudentList[0].lastName = 'Nazemi';
+
+    this.setState(newStudentList);
+  };
+
+  toggleListHandler = () => {
+    const newState = { ...this.state };
+    newState.toggleShow = !newState.toggleShow;
+
+    this.setState(newState);
+  };
   render() {
     return (
       <div className='App'>
@@ -16,11 +55,22 @@ class App extends Component {
           <img src={logo} className='App-logo' alt='logo' />
           <h2>{MY_PROJECT_NAME}</h2>
           <p>{this.prompt}</p>
+          <button onClick={this.clickHandler}>Change Student</button>
+          <button onClick={this.toggleListHandler}>Toggle List</button>
           <hr />
-          <Student firstName="Amir Hossein" lastName="Salighe Dar"/>
+          {this.state.toggleShow
+            ? this.state.studentList.map((student, index) => (
+                <Student
+                  key={index}
+                  firstName={student.firstName}
+                  lastName={student.lastName}
+                />
+              ))
+            : null}
+          {/* <Student firstName="Amir Hossein" lastName="Salighe Dar"/>
           <Student firstName="Amir Mehdi" lastName="Vaez"/>
           <Student firstName="Hassan" lastName="Beiki"/>
-          <Student firstName="Navid" lastName="Farahmand"/>
+          <Student firstName="Navid" lastName="Farahmand"/> */}
         </header>
       </div>
     );
--- src/components/student/Student.js
+++ src/components/student/Student.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import './Student.css';
 
 export default class Student extends Component {
-    
+    constructor(){
+        super();
+        this.state = {
+            hide : ''
+        }
+    }
+    hideStudentHandler=()=>{
+        this.setState({hide: 'hide'})
+    }
   render() {
     return (
-      <div className="student-row">
+      <div className={`student-row ${this.state.hide}`} onClick={this.hideStudentHandler}>
         <span className="student-title">First Name :</span><span>{this.props.firstName}</span>
         <span className="student-title">Last Name :</span><span>{this.props.lastName}</span>
       </div>
